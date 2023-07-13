9d3b7ab2ce275531d471be1e6254c2fe58f3f3f2
svyturys <tarento.sv@gmail.com>
2023-07-13 16:13:37 +0300

    rem2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -49,7 +49,6 @@ flatpickr('#datetime-picker', {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
