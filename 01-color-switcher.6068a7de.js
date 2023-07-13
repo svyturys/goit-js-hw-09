e65ea9667f39b80f087a9e57f2970bf099920362
svyturys <114387745+svyturys@users.noreply.github.com>
2023-07-13 13:14:04 +0000

    Deploying to gh-pages from @ svyturys/goit-js-hw-09@9d3b7ab2ce275531d471be1e6254c2fe58f3f3f2 🚀

---
+++ 01-color-switcher.6068a7de.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.6068a7de.js.map
---
+++ 01-color-switcher.71d8f4e4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,o=null;function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){o=setInterval(a,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.71d8f4e4.js.map
